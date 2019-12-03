69821ada10b19b7c2e4885c78a03345f13855097
YasinHafiz <52492642+YasinHafiz@users.noreply.github.com>
2019-12-03 11:13:35 +0300

    Add files via upload

---
+++ build/precache-manifest.f339b9611a5a4d8feb18573ef38874e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46e71ffbfc051049e4f7a96041002959",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28aa9a41978916225d7f",
+    "url": "/static/css/main.90f4e0de.chunk.css"
+  },
+  {
+    "revision": "d03b65ff26fac0268e75",
+    "url": "/static/js/2.118cdef8.chunk.js"
+  },
+  {
+    "revision": "28aa9a41978916225d7f",
+    "url": "/static/js/main.b83a1b2e.chunk.js"
+  },
+  {
+    "revision": "cf5cd37b0cf75f7011e4",
+    "url": "/static/js/runtime-main.cc1ccc8f.js"
+  }
+]);
